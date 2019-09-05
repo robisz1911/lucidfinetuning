6bc18e1416d7e6ac54dd7661cb394cda39e8980c
robisz1911 <49338407+robisz1911@users.noreply.github.com>
2019-09-05 02:08:45 +0200

    Update vis_script.sh

--- vis_script.sh
+++ vis_script.sh
@@ -3,27 +3,29 @@
 
 # visualize chosen layer from .pb files in 'training_x'
 
-######  NOTE : before running the script, delete 'pictures_x' folder ######
-
-mkdir pictures_x
-cp merge.py pictures_x
-
-rows=100                                              # the final image will have this many rows
-layer=Mixed_4c_Branch_3_b_1x1_act/Relu
+######  NOTE : before running the script, rename foldername to a non existing foldername ######
 
 #sed -i 's/COLUMNS = .*/COLUMNS = 15/' vis.py       # SET : number of neurons visualized       |THESE TWO-      |
 #sed -i 's/columns = .*/columns = 15/' merge.py     # SET : number of neurons visualized       |MUST BE THE SAME|
+rows=100
+foldername = picture_x
+mkdir $foldername
+cp merge.py $foldername
+
+                                             # the final image will have this many rows
+layer=Mixed_4c_Branch_3_b_1x1_act/Relu
+
 
 # for i in {1..46..5}
 for (( i = 1; i <= $rows; i++ ))                    
 do
                                                     # visualize, then copy .png to 'pictures_x'
 						    # set the layer after grep below
-    cat googlenet-node-names | grep $layer | python vis.py training_x/$i.pb - $i
-    mv $i.png pictures_x
+    cat googlenet-node-names | grep $layer | python vis.py $foldername/$i.pb - $i
+    mv $i.png $foldername
 
 done
                                                     # merge all images in 'pictures_x' - merged.png
 						    
-cd pictures_x
+cd $foldername
 python merge.py
