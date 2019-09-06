3cecf2af8a57a6bed9f748f25210837efe9b3075
robisz1911 <49338407+robisz1911@users.noreply.github.com>
2019-09-06 09:06:04 +0200

    Update train_vis_script.sh

--- train_vis_script.sh
+++ train_vis_script.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # THIS SCRIPT DOES:
-#     make training_x and pictures_x folders
-#     copy merge.py into pictures_x      | read merge.py's comments for description
+#     create one folder for images, and one for training files (folder_for_pictures,folder_for_trainings)
+#     copy merge.py into folder_for_pictures      |    read merge.py's comments for more details
 #     do the training - .pb files will be saved into training_x
-#     visualize chosen layer and create one image - .png files, and merged.png saved in pictures_x
+#     visualize chosen layer and create one image  - .png files, and merged.png saved in pictures_x
 # 
-# in merge.png:      1.pb is the default - imagenet                   | 1.png is the choosen layer visualized from imagenet
-#                    2.pb the network after (nb_epochs) epochs        | 2.png is the choosen layer visualized after (nb_epochs) epochs
-#                    n.pb the network after (n-1)*(nb_epochs) epochs  | n.png is the choosen layer visualized after (nb_epochs)*(n-1) epochs
+#         1.pb is the default - imagenet                     | 1.png is the choosen layer visualized from imagenet
+#         2.pb the network after (nb_epochs) epochs          | 2.png is the choosen layer visualized after (nb_epochs) epochs
+#         n.pb the network after [(n-1)*(nb_epochs)] epochs  | n.png is the choosen layer visualized after [(nb_epochs)*(n-1)] epochs
 #
-# commands      using sed -i (search and reaplace) you can set all the parameters from this script - or set them manually in train.py, vis.py, merge.py
+# commands      using sed -i (search and reaplace) you can set all the parameters from this script
 #               .sh files should have execute permission    |   chmod +x ./yourscriptname.sh
 #               run .sh file                                |            ./yourscriptname.sh  
 
-
-######  NOTE : before running the script, delete training_x, pictures_x folders ###################################################
+######  NOTE : before running the script, rename folder_for_pictures and folder_for_trainings to non existing foldernames ############
 
 #sed -i 's/nb_epoch = .*/nb_epoch = 1/' train.py   # SET : visualize after every n'th epochs
 #sed -i 's/COLUMNS = .*/COLUMNS = 5/' vis.py       # SET : number of neurons visualized       |THESE TWO-      |
@@ -33,7 +32,7 @@ layer=Mixed_4f_Branch_3_b_1x1_act/Relu
 
 
 
-for (( i = 1; i <= $rows; i++ ))                                    # SET : train for (np_epochs) epochs then visualize this many times
+for (( i = 1; i <= $rows; i++ ))
 do
     
     if [ $i == 1 ]; then                            # both variables set to False, so we do not finetune or load weights from previous training 
@@ -56,7 +55,6 @@ do
     mv $i.png $folder_for_pictures
 
 done
-                                                    # finally, we cd into pictures_x folder and run merge.py ( this should be executed next to the images )
 
 cd $folder_for_pictures
 python merge.py
