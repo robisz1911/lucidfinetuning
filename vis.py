51cfd5ff8558b315627ecb11ac48a12e07386dde
csillagm <csillagm@git.com>
2019-09-06 17:30:46 +0200

    added visualization parameters

--- vis.py
+++ vis.py
@@ -4,11 +4,13 @@ import scipy.misc
 from lucid.modelzoo.vision_base import Model
 import lucid.optvis.render as render
 import lucid.optvis.objectives as objectives
+import lucid.optvis.param as param
+
 
 MODEL_PATH, LAYER, OUTPUT_PREFIX = sys.argv[1:]
 # 'inceptionLucid.pb', 'average_pooling2d_9/AvgPool'
 
-COLUMNS = 5
+COLUMNS = 10
 
 class FrozenNetwork(Model):
     model_path = MODEL_PATH
@@ -20,25 +22,26 @@ network = FrozenNetwork()
 network.load_graphdef()
 
 if LAYER == "-":
+    height, width = 128, 128
     images = []
     layers = []
     for l in sys.stdin:
         layers.append(l.strip())
 
-    for layer in layers:
+    for layer in layers: 
         for i in range(COLUMNS):
+            param_f = lambda: param.image(height, fft=True, decorrelate=True)
             obj = objectives.channel(layer, i)
-            renders = render.render_vis(network, obj)
+            renders = render.render_vis(network, obj, param_f, thresholds=(2048,))
             assert len(renders)==1
             image = renders[0]
             assert len(image)==1
             image = image[0]
             images.append(image)
     images = np.array(images)
-    height, width = 128, 128
     rows = len(layers)
     print(images.shape)
-    assert images.shape == (rows * COLUMNS, 128, 128, 3)
+    assert images.shape == (rows * COLUMNS, height, width, 3)
     grid = (images.reshape(rows, COLUMNS, height, width, 3)
               .swapaxes(1,2)
               .reshape(height*rows, width*COLUMNS, 3))
