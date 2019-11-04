a9563c0bc20c337c754e0927686adf3b5a68b2ce
csillagm <marton.csillag.11@gmail.com>
2019-11-04 18:49:02 +0100

    script collects images in folders

--- neuron_catalog/vis_neuron.sh
+++ neuron_catalog/vis_neuron.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
 # download necessary frozen model files
-curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_flowers.pb
-curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_finetuned.pb
-curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_imagenet.pb
-mv googlenet_imagenet.pb googlenet_default.pb
+# curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_flowers.pb
+# curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_finetuned.pb
+# curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_imagenet.pb
+# mv googlenet_imagenet.pb googlenet_default.pb
 
-input="layers_name_channel.txt"
+input="layers_name_channel_relu.txt"
 
 while IFS= read -r line
 do
 
     vars=( $line )
-    layer=${vars[0]}"/Relu"
+    layer=${vars[0]}
     n_neurons=1
+
 #    n_neurons=${vars[1]}-1
 
     echo $layer
     echo $n_neurons
+
+    dir=${layer////-}
+    mkdir $dir
+
+
 # for number of neurons in layer
     for (( i=0; i<=n_neurons; i++ ))
     do
         echo $i
-        for weights in default finetuned flowers
+        for weights in default #finetuned flowers
         do
             python vis_neuron.py --MODEL_PATH=googlenet_$weights.pb --LAYER=$layer --NEURON_INDEX=$i
 
@@ -31,5 +37,7 @@ do
         #python merge_neuron.py $layer $i
 
     done
+
+    mv $dir_* $dir
 
 done < "$input"
