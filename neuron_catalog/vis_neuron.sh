a91738ef10cfec8230d7ab530042186a62b18900
robisz1911 <49338407+robisz1911@users.noreply.github.com>
2019-11-06 15:13:04 +0100

    rollback to Marci_version

--- neuron_catalog/vis_neuron.sh
+++ neuron_catalog/vis_neuron.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
 # download necessary frozen model files
-
-input=layers_name_channel_relu.txt
-folder_for_visualizing=test_neuron_vis
-
-
-mkdir $folder_for_visualizing
-cp $input $folder_for_visualizing/$input
-cp vis_neuron.py $folder_for_visualizing/vis_neuron.py
-cp merge_neuron.py $folder_for_visualizing/merge_neuron.py
-cd $folder_for_visualizing
-
-
-curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_flowers.pb
+# curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_flowers.pb
 curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_finetuned.pb
 curl -OL https://raw.githubusercontent.com/robisz1911/LUCID_RESULTS/master/single_results/deepdream/googlenet_imagenet.pb
-mv googlenet_imagenet.pb googlenet_default.pb
+# mv googlenet_imagenet.pb googlenet_default.pb
 
+input="layers_name_channel_concat.txt"
 
 while IFS= read -r line
 do
 
     vars=( $line )
     layer=${vars[0]}
-#    n_neurons=${vars[1]}-1
     n_neurons=1
+
+#    n_neurons=${vars[1]}-1
+
+    echo $layer
+    echo $n_neurons
 
     dir=${layer////-}
     mkdir $dir
@@ -34,22 +27,17 @@ do
 # for number of neurons in layer
     for (( i=0; i<=n_neurons; i++ ))
     do
-        echo $layer/$i/$n_neurons
-        for weights in default finetuned flowers
+        echo $i
+        for weights in default finetuned #flowers
         do
             python vis_neuron.py --MODEL_PATH=googlenet_$weights.pb --LAYER=$layer --NEURON_INDEX=$i
 
         done
 
-        python merge_neuron.py $layer $i
-        rm -rf *default*.png
-        rm -rf *finetuned*.png
-        rm -rf *flowers*.png
-        echo $layer"_"$i.png
-        mv ${layer////-}"_"$i.png $dir/${layer////-}"_"$i.png
+        #python merge_neuron.py $layer $i
 
     done
 
-#    mv $layer"_"$i.png $dir/$layer"_"$i.png
+    mv $dir* $dir
 
 done < "$input"
