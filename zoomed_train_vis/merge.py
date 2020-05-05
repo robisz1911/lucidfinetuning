d56cc02ae0977b737e037bef0b6c7a38922a48ee
robisz1911 <robisz1911@gmail.com>
2020-05-05 16:48:20 +0200

    upload celeba trainer

---
+++ zoomed_train_vis/merge.py
@@ -0,0 +1,52 @@
+# iterate over png files in the current folder, and merge them into one png (merged.png) 
+# PARAMETER :
+#                       column    :    SAME as the COLUMN in vis.py ( the number of neurons visualized )
+# NOTE : if you change the size of the generated images(in vis.py), change height and width here too!
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+import sys
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--column", help='number of neurons in each layer', type=int)
+parser.add_argument("--name", help='name of the saved file', type=str, default="merged")
+
+FLAGS = parser.parse_args()
+
+name = FLAGS.name
+column = FLAGS.column
+
+
+def merge():
+    number_of_pictures = 0
+    for filename in os.listdir():
+        if filename.endswith(".png"):
+            x = filename.zfill(40)
+            os.rename(filename,x)
+            number_of_pictures += 1     
+    height = 256
+    #column = 5 # must be the same as COLUMN in vis.py ( the number of neurons we wanna to visualize )
+#    column_arg = sys.argv[1:]
+#    print(column_arg)
+#    column = int(column_arg)
+    width = 256*column
+    merged = Image.new('RGB', (width, height*number_of_pictures))
+    iterator = 0
+    for filename in sorted(os.listdir()):
+        if filename.endswith(".png"):
+            print(filename)
+            y = Image.open(filename)
+            ###
+            draw = ImageDraw.Draw(y)
+            #font = ImageFont.truetype("sans-serif.ttf", 16)
+            draw.text((0, 0),filename[-12:-7],(255,255,255))
+            
+            ###
+            merged.paste(y, (0, iterator*height))
+            iterator += 1
+    return merged
+
+merge().save(name+'.png')
---
+++ zoomed_train_vis/vis_steps_neuron.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+input="layers_neuron_list.txt"
+savefolder=test_vis
+
+mkdir $savefolder
+
+while IFS= read -r line
+do
+#read txt line by line
+#each line is a space separated string
+#example line:    "layer 1,2,3,4,5"
+#separate by space into 2 variable
+    vars=( $line )
+    layer=${vars[0]}
+    neuron_list=${vars[1]}
+#iterate over the second variable below(neuron indexes)
+    for neuron_index in $(echo $neuron_list | sed "s/,/ /g")
+    do
+        #echo "$layer"
+        #echo "$i"
+#for each layer name and neuron index
+#iterate over the .pb files
+
+        for (( i = 0; i <= 20; i++ ))
+        do
+            echo $i
+            python vis_neuron.py --MODEL_PATH=$i.pb --LAYER=$layer --NEURON_INDEX=$neuron_index
+        done
+
+        for (( i = 21; i <= 40; i+=3 ))
+        do
+            echo $i
+            python vis_neuron.py --MODEL_PATH=$i.pb --LAYER=$layer --NEURON_INDEX=$neuron_index
+        done
+
+        for (( i = 45; i <= 100; i+=5 ))
+        do
+            python vis_neuron.py --MODEL_PATH=$i.pb --LAYER=$layer --NEURON_INDEX=$neuron_index
+        done
+
+        for (( i = 110; i <= 200; i+=10 ))
+        do
+            python vis_neuron.py --MODEL_PATH=$i.pb --LAYER=$layer --NEURON_INDEX=$neuron_index
+        done
+
+        for (( i = 250; i <= 600; i+=50 ))
+        do
+            python vis_neuron.py --MODEL_PATH=$i.pb --LAYER=$layer --NEURON_INDEX=$neuron_index
+        done
+
+
+
+
+
+
+        echo "merge.py-start"
+        python merge.py --column=1 --name=merged_steps${layer////-}$neuron_index
+        echo "merge.py-end"
+        mv merged_steps${layer////-}$neuron_index.png $savefolder/
+        rm *.png
+    done
+
+    
+
+done < "$input"
